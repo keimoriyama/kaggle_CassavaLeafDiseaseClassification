7f9077ffded1bf18b4a33c3318d39f368a2250f2
kei moriyama <keischwiiz@gmail.com>
2021-01-18 16:57:41 +0900

    add memo

--- code/ef.py
+++ code/ef.py
@@ -1,9 +1,9 @@
 import timm
 
-ef_model = timm.create_model("vit_base_patch32_384", pretrained = True)
+ef_model = timm.create_model("resnet50", pretrained = True)
 from torchsummary import summary
 print(summary(ef_model, (3, 512, 512)))
 
 import torch
 
-torch.save(ef_model.state_dict(), "./pretrained_ef_model.pth")
+torch.save(ef_model.state_dict(), "./pretrained_resNet.pth")
